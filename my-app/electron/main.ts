e33d6ddf8586b75b61ca2570e0bdc4fa23462434
hctynch <hctynch@ncsu.edu>
2025-04-12 22:40:45 -0400

    ...

--- my-app/electron/main.ts
+++ my-app/electron/main.ts
@@ -166,6 +166,17 @@ autoUpdater.on('error', (error) => {
   }
 });
 
+// Force `app.isPackaged` to `true` if running from an ASAR archive
+if (process.resourcesPath.includes('app.asar')) {
+  Object.defineProperty(app, 'isPackaged', {
+    get() {
+      return true;
+    },
+  });
+  process.env.NODE_ENV = 'production';
+  console.log('Forcing app.isPackaged = true');
+}
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
